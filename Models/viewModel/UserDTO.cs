41ee48fd1ad3511ae9cb5de36439e3cd9287a6bb
hazeldex <152042208+hazeldex@users.noreply.github.com>
2023-12-22 23:32:33 -0600

    UserDTO

--- Models/viewModel/UserDTO.cs
+++ Models/viewModel/UserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,26 @@ namespace LaFarmacia.Models.viewModel
 {
     public class UserDTO
     {
+        [Required]
+        [Display(Name = "Identificacion")]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nombre completo")]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Correo electronnico")]
         public string Email { get; set; }
+        [Required]
+        [Display(Name = "Tipo usuario")]
         public int RolId { get; set; }
+        [Required]
+        [Display(Name = "Descripcion")]
         public string RolDescription { get; set; }
+        [Required]
+        [Display(Name = "Estado")]
         public bool State { get; set; }
+        [Required]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
     }
 }
